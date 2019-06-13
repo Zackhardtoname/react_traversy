2ede7cc04f2309bf55221d0aee3bfe79824d3bb3
Zackhardtoname <zackL@bu.edu>
2019-06-13 11:02:48 -0400

    converting almost everything to function-based components

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,27 +1,25 @@
-import React, {Component} from 'react';
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from "prop-types";
 
-export class NavBar extends Component {
-    static defaultProps = {
-        title: "Github Finder",
-        icon: "fab fa-github"
-    }
+const NavBar = ({icon, title}) => {
+    return (
+        <nav className='navbar bg-primary'>
+            <h1>
+                {/*If you split this in two lines, you won't have a space in the middle*/}
+                <i className={icon} /> {title}
+            </h1>
+        </nav>
+    );
+}
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired,
-    }
+NavBar.defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+}
 
-    render() {
-        return (
-            <nav className='navbar bg-primary'>
-                <h1>
-                    {/*If you split this in two lines, you won't have a space in the middle*/}
-                    <i className={this.props.icon} /> {this.props.title}
-                </h1>
-            </nav>
-        );
-    }
+NavBar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
 }
 
 export default NavBar;
